7a9a20b29f45199e0f89f2ea2ec5e4967c3a9d04
Mohit Singh <ashamohit78612@gmail.com>
2021-03-24 22:57:29 +0530

    this is the final commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div>
-      
+      <p>this is the modified version of the react with the change
+        of only one thing which is called the addition of final
+      </p>
     </div>
   );
 }
